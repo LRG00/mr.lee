3ae3bc88c022d80dde9fe0312094c73ecde448d3
LRG00 <158097628@qq.com>
2019-11-28 19:42:18 +0800

    flex 布局

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,25 @@
 <template>
-<div class="home">
-  <icon-svg name="daoru"></icon-svg>
+<div class="page-home">
+  <!-- <icon-svg name="daoru"></icon-svg>
   <icon-svg :name="iconList[0]"></icon-svg>
-  <span>哈哈</span>
+  <span>哈哈</span> -->
+  <div class="page-home-container">
+    <div class="header-container">
+      <div class="header lrg-pbk lrg-jz">
+        <div class="header-logo">
+          Mr.Lee
+        </div>
+      </div>
+    </div>
+    <div class="main lrg-pbk lrg-jz">
+      <div class="main-nav">main-nav</div>
+      <div class="main-content">main-content</div>
+      <div class="main-right">main-right</div>
+    </div>
+    <div class="footer-container">
+      <div class="footer lrg-pbk lrg-jz">footer</div>
+    </div>
+  </div>
 </div>
 </template>
 <script>
@@ -14,7 +31,60 @@
   }
 </script>
 <style lang="less">
-  .home {
+  .page-home {
     border: 1px solid pink;
+    display: flex;
+    flex-direction: column;
+    .page-home-container {
+      .header-container {
+        background: rgba(255, 255, 255, 0.6);
+        width: 100%;
+        position: fixed;
+        .header {
+          height: 60px;
+          // background: red;
+          .header-logo {
+            background-color: rgba(255, 255, 255, 0.6);
+            color: #1b94f5;
+            font-size: 30px;
+            font-weight: 700;
+            padding: 10px;
+            width: 300px;
+            mix-blend-mode: screen;
+          }
+        }
+      }
+      .footer-container {
+        background: pink;
+        .footer {
+          height: 60px;
+          background: red;
+        }
+      }
+
+      .main {
+        padding-top: 60px;
+        height: 1500px;
+        display: flex;
+        flex-direction: row;
+        .main-nav {
+          width: 160px;
+          background: rebeccapurple;
+          margin-right: 15px;
+          position: fixed;
+          height: 300px;
+        }
+        .main-right {
+          width: 240px;
+          background: rebeccapurple;
+          margin-left: 15px
+        }
+        .main-content {
+          flex: 1;
+          margin-left: 175px;
+          background: rgb(216, 207, 224);
+        }
+      }
+    }
   }
 </style>
