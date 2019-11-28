d5f9fd25d2debf126481060a6f5ea1d314ba1185
leeruigan <leeruigan@gmail.com>
2019-11-28 23:00:33 +0800

    首页

---
+++ components/MainNav.vue
@@ -0,0 +1,58 @@
+<!--
+ * @Author: liruigang
+ * @Date: 2019-11-28 20:50:43
+ * @LastEditors: liruigang
+ * @LastEditTime: 2019-11-28 22:17:59
+ * @UI: 
+ -->
+<template>
+  <div class="nav">
+    <a @click="handleClick(idx)" :class="index === idx ? 'active' : ''" :key="idx" v-for="(item, idx) in navList" href="#"><icon-svg name="up"></icon-svg> <span>{{item}}</span></a>
+    <!-- <span href="#"><icon-svg name="daoru"></icon-svg> <span>我的收藏</span></a>
+    <a class="active" href="#"><icon-svg name="daoru"></icon-svg> <span>我的收藏</span></a>
+    <a href="#"><icon-svg name="daoru"></icon-svg> <span>我的收藏</span></a>
+    <a href="#"><icon-svg name="daoru"></icon-svg> <span>我的收藏</span></a> -->
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      index: 0,
+      navList: ['我的收藏', '文章列表', '我的code', '云裳爆款']
+    }
+  },
+  methods: {
+    handleClick (idx) {
+      this.index = idx
+    }
+  },
+}
+</script>
+<style lang="less">
+  .nav {
+    width: 100%;
+
+    a {
+      height: 45px;
+      line-height: 45px;
+      display: block;
+      padding: 0 10px;
+      transition: all 0.3s ease;
+      color: rgba(0, 0, 0, 0.54);
+      svg {
+        vertical-align: middle;
+        font-size: 18px;
+      }
+    }
+
+    a:hover {
+      color: rgb(151, 191, 247);
+    }
+
+    .active {
+      color: rgb(151, 191, 247);
+      background: #fff
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: liruigang
+ * @Date: 2019-11-28 20:37:10
+ * @LastEditors: liruigang
+ * @LastEditTime: 2019-11-28 22:36:35
+ * @UI: 
+ -->
 <template>
 <div class="page-home">
   <!-- <icon-svg name="daoru"></icon-svg>
@@ -12,8 +19,12 @@
       </div>
     </div>
     <div class="main lrg-pbk lrg-jz">
-      <div class="main-nav">main-nav</div>
-      <div class="main-content">main-content</div>
+      <div class="main-nav">
+        <main-nav></main-nav>
+      </div>
+      <div class="main-content">
+        <h2 class="textured-type">Helmeted Hornbill</h2>
+      </div>
       <div class="main-right">main-right</div>
     </div>
     <div class="footer-container">
@@ -24,7 +35,11 @@
 </template>
 <script>
   import Icon from '@/icons'
+  import MainNav from '../components/MainNav'
   export default {
+    components: {
+      MainNav
+    },
     created () {
       this.iconList = Icon.getNameList()
     },
@@ -32,7 +47,6 @@
 </script>
 <style lang="less">
   .page-home {
-    border: 1px solid pink;
     display: flex;
     flex-direction: column;
     .page-home-container {
@@ -44,7 +58,6 @@
           height: 60px;
           // background: red;
           .header-logo {
-            background-color: rgba(255, 255, 255, 0.6);
             color: #1b94f5;
             font-size: 30px;
             font-weight: 700;
@@ -55,35 +68,106 @@
         }
       }
       .footer-container {
-        background: pink;
+        background: #f8f8f8;
+        width: 100%;
         .footer {
           height: 60px;
-          background: red;
+          line-height: 60px;
+          text-align: center;
         }
       }
 
       .main {
-        padding-top: 60px;
+        padding-top: 75px;
+        padding-bottom: 15px;
         height: 1500px;
         display: flex;
         flex-direction: row;
         .main-nav {
           width: 160px;
-          background: rebeccapurple;
           margin-right: 15px;
           position: fixed;
-          height: 300px;
         }
         .main-right {
           width: 240px;
-          background: rebeccapurple;
+          background: rgba(255, 255, 255, 0.6);
           margin-left: 15px
         }
         .main-content {
           flex: 1;
           margin-left: 175px;
-          background: rgb(216, 207, 224);
+          // background: rgba(255, 255, 255, 0.6);
+          background: #000;
+          .textured-type {
+            font-weight: 600px;
+            font-size: 40px;
+            background: url('../static/repeat-white.png');
+            -webkit-text-fill-color: transparent;
+            color: #fff;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            -webkit-animation: scratchy 0.253s linear forwards infinite;
+            -moz-animation: scratchy 0.253s linear forwards infinite;
+            animation: scratchy 0.253s linear forwards infinite;
+          }
         }
+@-webkit-keyframes scratchy {
+  0% {
+    background-position: 0 0; }
+  25% {
+    background-position: 0 0; }
+  26% {
+    background-position: 20px -20px; }
+  50% {
+    background-position: 20px -20px; }
+  51% {
+    background-position: 40px -40px; }
+  75% {
+    background-position: 40px -40px; }
+  76% {
+    background-position: 60px -60px; }
+  99% {
+    background-position: 60px -60px; }
+  100% {
+    background-position: 0 0; } }
+@-moz-keyframes scratchy {
+  0% {
+    background-position: 0 0; }
+  25% {
+    background-position: 0 0; }
+  26% {
+    background-position: 20px -20px; }
+  50% {
+    background-position: 20px -20px; }
+  51% {
+    background-position: 40px -40px; }
+  75% {
+    background-position: 40px -40px; }
+  76% {
+    background-position: 60px -60px; }
+  99% {
+    background-position: 60px -60px; }
+  100% {
+    background-position: 0 0; } }
+@keyframes scratchy {
+  0% {
+    background-position: 0 0; }
+  25% {
+    background-position: 0 0; }
+  26% {
+    background-position: 20px -20px; }
+  50% {
+    background-position: 20px -20px; }
+  51% {
+    background-position: 40px -40px; }
+  75% {
+    background-position: 40px -40px; }
+  76% {
+    background-position: 60px -60px; }
+  99% {
+    background-position: 60px -60px; }
+  100% {
+    background-position: 0 0; } }
       }
     }
   }
