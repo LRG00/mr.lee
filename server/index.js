b89a299bb2c0139d4285f26dfe89fcbd288b64c0
LRG00 <158097628@qq.com>
2019-12-23 16:32:25 +0800

    pm2 启动

--- server/index.js
+++ server/index.js
@@ -1,18 +1,14 @@
-import Koa from 'koa'
-import conf from './config'
+const Koa = require('koa')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
 import { resolve } from 'path'
-import { Nuxt, Builder} from 'nuxt'
 import R from 'ramda'
+const app = new Koa()
 
 const r = (path) => resolve(__dirname, path)
 const MIDDLEWARE = ['database', 'general', 'router']
-const app = new Koa()
 
 const useMiddleware = (app) => {
-  // 中间件的个数不定，通过 Ramda 的特性，从右往左进行函数组合，右侧函数的返回结果总是左侧函数的输入参数
-  // R.map(console.log)([1, 2, 3])
-  // MIDDLEWARE 数组交给了 R.map
-  // 分别拿到的单个数组中的值，我们可以通过 R.compose 再次进行组合。
   return R.map(
     R.compose(
       R.map((i) => i(app)),
@@ -21,32 +17,41 @@ const useMiddleware = (app) => {
     )
   )
 }
+// Import and Set Nuxt.js options
+const config = require('../nuxt.config.js')
+config.dev = app.env !== 'production'
 
-const start = async () => {
-  let config = require('../nuxt.config.js')
-  config.dev = !(app.env === 'production')
-  const nuxt = await new Nuxt(config)
+async function start () {
+  // Instantiate nuxt.js
+  const nuxt = new Nuxt(config)
+  
+  const {
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000
+  } = nuxt.options.server
 
-  if (conf.env !== 'production') {
-    try {
-      await new Builder(nuxt).build()
-    } catch (e) {
-      console.error(e)
-      process.exit(1)
-    }
+  // Build in development
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  } else {
+    await nuxt.ready()
   }
 
   await useMiddleware(app)(MIDDLEWARE)
 
-  app.use(ctx => {
+  app.use((ctx) => {
     ctx.status = 200
-    ctx.respond = false // Mark request as handled for Koa
+    ctx.respond = false // Bypass Koa's built-in response handling
     ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
     nuxt.render(ctx.req, ctx.res)
   })
 
-  app.listen(conf.port, conf.host)
-  console.log('Server listening on ' + conf.host + ':' + conf.port) // eslint-disable-line no-console
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
 }
 
 start()
